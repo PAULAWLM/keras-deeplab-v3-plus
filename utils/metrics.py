cf1ca3a26a6ffb40d10c9443c6a4edad40560e5e
PAULAWLM <paulawlm@icloud.com>
2021-09-23 12:33:16 +0200

    utils aus Amazing-Semantic-Segmentation

---
+++ utils/metrics.py
@@ -0,0 +1,32 @@
+"""
+The implementation of some metrics based on Tensorflow.
+
+@Author: Yang Lu
+@Github: https://github.com/luyanger1799
+@Project: https://github.com/luyanger1799/amazing-semantic-segmentation
+
+"""
+import tensorflow as tf
+from tensorflow.python.keras.metrics import Precision, SensitivityAtSpecificity
+
+
+class MeanIoU(tf.keras.metrics.MeanIoU):
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        # sparse code
+        y_true = tf.argmax(y_true, axis=-1)
+        y_pred = tf.argmax(y_pred, axis=-1)
+        return super(MeanIoU, self).update_state(y_true, y_pred, sample_weight)
+
+class Presicion(tf.keras.metrics.Precision):
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        # sparse code
+        y_true = tf.argmax(y_true, axis=-1)
+        y_pred = tf.argmax(y_pred, axis=-1)
+        return super(Precision, self).update_state(y_true, y_pred, sample_weight=sample_weight)
+
+class Sensitivity(tf.keras.metrics.SensitivityAtSpecificity):
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        # sparse code
+        y_true = tf.argmax(y_true, axis=-1)
+        y_pred = tf.argmax(y_pred, axis=-1)
+        return super(SensitivityAtSpecificity, self).update_state(y_true, y_pred, sample_weight=sample_weight)
